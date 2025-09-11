40836bd441170295728b53413360028eb33f7d4d
watersmakeradmin <admin@watersmaker.be>
2025-09-11 18:22:25 +0200

    Update events.js

--- events.js
+++ events.js
@@ -11,16 +11,16 @@ const events = [
     { title: "Theeproeverij - 19:00", date: "2025-06-19" },
     { title: "Theeproeverij - 19:00", date: "2025-07-17" },
     { title: "Theeproeverij - 19:00", date: "2025-08-21" },
-    { title: "Theeproeverij - 19:00", date: "2025-09-18" },
+    { title: "Theeproeverij - 19:30", date: "2025-09-18" },
     { title: "Thee-kennismaking Seniorenresidentie Meulenberg op autoluwe zondag - 15:00", date: "2025-09-21" },
     {
         title: "Een Kopje Zen - Maanfestival - 19:00",
         date: "2025-10-01",
         link: "https://www.gemeentebkz.be/activiteiten/detail/1103/een-kopje-zen-maanfestival"
     },
-    { title: "Theeproeverij - 19:00", date: "2025-10-16" },
-    { title: "Theeproeverij - 19:00", date: "2025-11-20" },
-    { title: "Theeproeverij - 19:00", date: "2025-12-18" }
+    { title: "Theeproeverij - 19:30", date: "2025-10-16" },
+    { title: "Theeproeverij - 19:30", date: "2025-11-20" },
+    { title: "Theeproeverij - 19:30", date: "2025-12-18" }
 
 
 
